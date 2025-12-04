8768da172fed60b4619def0b201dc3978f314b82
p.Karl <pkarlinsky@profit.com.ua>
2025-12-04 11:05:28 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+//import { defineConfig } from 'vite'
+//import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+//export default defineConfig({
+  //plugins: [vue()],
+//})
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      // всі запити на /api будуть прокситись на твій бекенд
+      '/api': {
+        target: 'https://personal-card-pied.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
